de3012e1aa7da4842f2824ac86e4a91c869275df
Parth Nautiyal <parthnautiyal002@gmail.com>
2021-07-20 08:24:18 +0530

    second

---
+++ Week 1/linear_search.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve()
+{
+    int n; //size
+    cin>>n;
+    int A[n];
+
+    for(int i=0;i<n;i++)
+        cin>>A[i]; //array
+
+    int key,flag=0,cmp=0;
+    cin>>key; //key element
+    
+    for(int i=0;i<n;i++)
+    {
+        if(key==A[i])
+        {
+            flag=1;
+            break;
+        }
+        cmp++;
+    }
+    if(flag)
+        cout<<"Present "<<cmp+1<<"\n";
+    else 
+        cout<<"Not Present "<<n<<"\n";
+
+}
+
+int main()
+{
+    int t;
+    cin>>t; //test cases
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
